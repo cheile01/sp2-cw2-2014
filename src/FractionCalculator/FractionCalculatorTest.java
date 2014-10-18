529e8cbcfb455296fa9fc6c611ea0c3175c37c38
cheile01 <cheile01@dcs.bbk.ac.uk>
2014-10-18 18:10:27 +0100

    Test package setup

---
+++ src/FractionCalculator/FractionCalculatorTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package FractionCalculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author christianheiler
+ *
+ */
+public class FractionCalculatorTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
